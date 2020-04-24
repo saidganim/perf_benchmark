acea1c1ea4b9a88de07ea9ae3a18f7ce95567599
Said Musaev <cpu808694@gmail.com>
2020-04-24 08:22:54 +0200

    second commit :)

--- bad_file.c
+++ bad_file.c
@@ -9,6 +9,9 @@
 #include <linux/types.h>
 
 
+#define TENREP(a) a a a a a a a a a a 
+#define HUNDRREP(a) TENREP(a) TENREP(a) TENREP(a) TENREP(a) TENREP(a) TENREP(a) TENREP(a) TENREP(a) TENREP(a) TENREP(a)
+
 MODULE_AUTHOR("Saidgani Musaev <TUD SE/chair>");
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Just small benchmark code");
@@ -49,6 +52,8 @@ int __init __attribute((optimize("O0")))my_init(void)
 {
     volatile unsigned int msr;
     volatile unsigned int low, high;
+    volatile unsigned int lowb, highb;
+    volatile unsigned int lowa, higha;
     uint64_t rd, rd2;
     int i;
     volatile int x1, x2;
@@ -62,29 +67,66 @@ int __init __attribute((optimize("O0")))my_init(void)
     // asm volatile("mfence");
     // buff[0] = 0x0;
     // asm volatile("mfence");
-    for(exp = 0; exp < 10000000; ++exp){
+    for(exp = 0; exp < 20000000; ++exp){
         msr = 0x187;
-        low = 0x4201A2; // 0x420e01 | (0x1<<23) | (0x1<<24)
-        high = 0x0;
+        // low = 0x4204A3 | (0x4<<24); // 0x420e01 | (0x1<<23) | (0x1<<24)
+        low = 0x4201A2; // RESOURCE_STALLS.ANY
+        // low = 0x42010E | (0x1<<23) | (0x1<<24); // UOPS_ISSUED.STALL_CYCLES
+        // low = 0x4204A3 | (0x4<<24); // CYCLE_ACTIVITY.TOTAL_STALLS        high = 0x0;
         wrmsr0(msr, 0, 0);
         wrmsr0(0xC2, 0, 0);
         wrmsr0(msr, low, high);    
-        asm volatile("mfence");
-        rd = rdmsr0(0x30a);
+        // asm volatile("mfence");
+        // rd = rdmsr0(0x30a);
+        asm volatile(
+            "\tmovq $0x30a, %%rcx\n"
+            "\trdmsr\n"
+            "\tmfence\n"
+            "\tmovl %%eax,%0\n"
+            "\tmovl %%edx, %1\n"
+            "\tmovq $200, %%rsi\n"
+            // "\tloop_begin:\n"
+           HUNDRREP("\tmovl %%eax,%0\n" "\tsfence\n" 
+            // "\tsfence\n"
+            "\tdecq %%rsi\n"
+            "\tdecq %%rsi\n"
+            "\tdecq %%rsi\n"
+            "\tdecq %%rsi\n"
+            "\tdecq %%rsi\n"
+            "\tdecq %%rsi\n"
+            "\tdecq %%rsi\n"
+            "\tdecq %%rsi\n"
+            "\tdecq %%rsi\n"
+            "\tdecq %%rsi\n"
+            "\tdecq %%rsi\n"
+            "\tdecq %%rsi\n"
+            "\tmovl %%edx, %1\n"
+            )
+            // "\tjnz loop_begin\n"
+            "\tmfence\n"
+            "\tmovq $0xC2, %%rcx\n"
+            "\trdmsr\n"
+            "\tmovl %%eax, %2\n"
+            "\tmovl %%edx, %3\n"
+            :"=m"(lowb), "=m"(highb),
+             "=m"(lowa), "=m"(higha)::"memory"
+        );
         // #pragma GCC unroll 5
-        for(i = 0; i < 50;){
-            // msr = i+100;
-            sb[i] = i*4+100;
-            // asm volatile("mfence");
-            sb[i+1] = i*4+100;
+        // for(i = 0; i < 50;){
+        //     // msr = i+100;
+        //     sb[i] = i*4+100;
+        //     // asm volatile("mfence");
+        //     sb[i+1] = i*4+100;
 
-            i = i+1;
-        }        
+        //     i = i+1;
+        // }        
             // asm volatile("mfence");
-            rd2 = rdmsr0(0x30a);
+            // rd2 = rdmsr0(0x30a);
             
             // rd = 0;
-        if(minval > (rd2-rd)) minval = rd2-rd;
+        rd = lowb | (highb<<32);
+        rd2 = lowa | (higha<<32);
+        if(minval > (rd2-rd)) minval = (uint64_t)rd2-(uint64_t)rd;
     }
         // msr;
         // msr;
@@ -114,7 +156,7 @@ int __init __attribute((optimize("O0")))my_init(void)
     // rd2 = rdpmc_actual_cycles(); //rdpmc0(1<<30);
     // asm volatile("mfence");
     msr = 0x187;
-    printk(KERN_ALERT "SAID:::val: %lu\n", minval);
+    printk(KERN_ALERT "SAID:::val: %lu;;; rd=%lu ; rd2=%lu\n", minval, rd, rd2);
     wrmsr0(msr, 0, 0);
     wrmsr0(0xC2, 0, 0);
     put_cpu();
--- build_and_run.sh
+++ build_and_run.sh
@@ -1,4 +1,4 @@
 make clean
 make
 sudo insmod bad_file.ko || true
-sudo dmesg | tail -n10 | grep "SAID::"
+sudo dmesg | grep "SAID::" | tail -n1
